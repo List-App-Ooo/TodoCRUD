6bc9c7557ec47c3b18179bf2a4752043d4717def
Daniel Hopson <hdhopson88@yahoo.com>
2020-12-30 14:09:01 -0800

    Creates todo repo

---
+++ TodoCRUD/Data/TodoRepo.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using TodoCRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoCRUD.Data
+{
+    public class TodoRepo : ITodoRepo
+    {
+        private TodoContext _context;
+
+        public TodoRepo(TodoContext context)
+        {
+            this._context = context;
+        }
+        public async Task<List<TodoItemModel>> GetTodoItems(Guid listId)
+        {
+            var query = _context.Todos.Where(m => m.ListId == listId).OrderBy(d => d.TimeStamp);
+            return await query.ToListAsync<TodoItemModel>();
+        }
+
+        public async Task<int> GetTotal(Guid listId)
+        {
+            return await _context.Todos.CountAsync(m => m.ListId == listId);
+        }
+    }
+}
