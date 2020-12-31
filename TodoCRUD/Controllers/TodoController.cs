41113be6324940db8a5a5cbaf0ae8b0ca74ba818
Daniel Hopson <hdhopson88@yahoo.com>
2020-12-30 23:13:53 -0800

    Creates todo controller to utilize todo service

---
+++ TodoCRUD/Controllers/TodoController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TodoCRUD.Models;
+using TodoCRUD.Services;
+
+namespace TodoCRUD.Controllers
+{
+    [ApiController]
+    [Route("api/todos")]
+    public class TodoController : ControllerBase
+    {
+        private readonly ITodoService _todoService;
+
+        public TodoController(ITodoService todoService)
+        {
+            this._todoService = todoService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetTodos(Guid listId)
+        {
+            var result = await _todoService.GetTodos(listId);
+            return Ok(result);
+        }
+    }
+}
